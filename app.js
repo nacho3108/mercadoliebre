b26320f6bd4cfe989b04bbf0eebf5c1b8343730b
nacho3108 <nacho3108@gmail.com>
2021-05-20 18:10:33 -0300

    Varios toques

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3031;
 
 app.use(express.static("public"));
 
